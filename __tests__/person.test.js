03680cd11c4325414e3a5f49873b1019278dd9cd
Grace Lee <gracexsnoopy@gmail.com>
2022-10-06 16:43:45 -0700

    Refactor code

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -1,17 +1,19 @@
 import Person from '../src/js/person.js'
 
 describe('Person', () => {
+  let newPerson;
+
+  beforeEach(() => {
+    newPerson = new Person(45, 80);
+  });
 
   test('should correctly assign each property of the Person class with the value assigned', () => {
-    const newPerson = new Person(45, 80);
     expect(newPerson.age).toEqual(45);
     expect(newPerson.lifeExpectancy).toEqual(80);
   });
 
   test('should correctly divide the age and round it to the nearest integer to determine age in Mercury years', () => {
-    const newPerson = new Person(45, 80);
     let mercuryAge = newPerson.mercuryAge(newPerson);
     expect(mercuryAge).toEqual(188);
   });
-
 });
